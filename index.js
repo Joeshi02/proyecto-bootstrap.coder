319a9c8d34e559776f7ec89b18ec8003c5fb015b
joacoEscobar <joaco.escobar.h@gmail.com>
2023-06-10 17:39:44 -0300

    arreglos en header

---
+++ index.js
@@ -0,0 +1,5 @@
+let numeroIngresado = parseInt(prompt(" ingresar un numero: "))
+for (let i = 1; i<= 10; i++) {
+    let resultado = numeroIngresado * i
+    alert(numeroIngresado+ " * " + i + "=" + resultado)
+}

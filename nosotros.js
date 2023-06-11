5a7ccc0e12d8945b64a35cd21a30ece46c46244a
joacoEscobar <joaco.escobar.h@gmail.com>
2023-06-11 19:23:28 -0300

    js

---
+++ nosotros.js
@@ -0,0 +1,5 @@
+let entrada = prompt ("ingresar un dato")
+while (entrada !== "ESC") {
+    alert ('el usuario ingreso: '+ entrada)
+    entrada = prompt ('ingresar otro dato: ')
+}
---
+++ productos.js
@@ -0,0 +1,6 @@
+ let numero = 0;
+ do{
+    numero = prompt("Ingresar Número");
+    console.log(numero);
+    
+}while(parseInt(numero));

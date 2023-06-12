7fc722409f224d6e1b20b7461351e633d8c83867
joacoEscobar <joaco.escobar.h@gmail.com>
2023-06-12 17:00:46 -0300

    añadimos un condicional en productos.js

--- productos.js
+++ productos.js
@@ -1,6 +1,93 @@
- let numero = 0;
- do{
-    numero = prompt("Ingresar Número");
-    console.log(numero);
+let ciudad = prompt("Para calcular envio ingrese su provincia: (ejemplo: buenos aires)")
+switch (ciudad) {
+    case 'catamarca':
+        alert("el valor es 2500 ")
+        break;
     
-}while(parseInt(numero));
+    case 'buenos aires':
+        alert("el valor es 1000")
+        break;
+        
+    case 'chaco':
+        alert("el valor es 2000 ")
+        break;
+
+    case 'chubut':
+        alert("el valor es 1800")
+        break;
+
+    case 'cordoba':
+        alert("el valor es 1600 ")
+        break;
+
+    case 'corrientes':
+        alert("el valor es  800")
+        break;
+
+    case 'entre rios':
+        alert("el valor es  1200")
+        break;
+
+    case 'formosa':
+        alert("el valor es 2500")
+        break;
+
+    case 'jujuy':
+        alert("el valor es 2500 ")
+        break;
+
+    case 'la pampa':
+        alert("el valor es 2100 ")
+        break;
+
+    case 'la rioja':
+        alert("el valor es 2200")
+        break;
+
+    case 'mendoza':
+        alert("el valor es 2800 ")
+        break;
+
+    case 'misiones':
+        alert("el valor es 3000")
+        break;
+
+    case 'neuquen':
+        alert("el valor es 1800 ")
+        break; 
+    case 'rio negro':
+        alert("el valor es 3500")
+        break;
+
+    case 'salta':
+        alert("el valor es 2500")
+        break;
+
+    case 'san juan':
+        alert("el valor es 2900")
+        break;
+
+    case 'san luis':
+        alert("el valor es 2900")
+        break;
+
+    case 'santa cruz':
+        alert("el valor es 1400")
+        break;
+
+    case 'santa fe':
+        alert("el valor es 1600")
+        break;
+
+    case 'santiago del estero':
+        alert("el valor es 1800")
+        break;
+
+    case 'tierra del fuego':
+        alert("el valor es gratis")
+        break;
+
+    case 'tucuman':
+        alert("el valor es 3000")
+        break;             
+}

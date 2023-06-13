7883e3ccc79189458fa1aa7b95f4852c068b0fd3
joacoEscobar <joaco.escobar.h@gmail.com>
2023-06-13 20:13:54 -0300

    arreglos js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-let numeroIngresado = parseInt(prompt(" ingresar un numero: "))
-for (let i = 1; i<= 10; i++) {
-    let resultado = numeroIngresado * i
-    alert(numeroIngresado+ " * " + i + "=" + resultado)
-}
+// //circuito 1
+// let precioIngresado = Number(prompt(" ingresar el valor del producto: "))
+// for (let i = 0.30; i<= 1; i++) {
+//     let resultado = precioIngresado * i
+//     alert("El valor del envio es " + "=" + resultado )
+// }
+
+//circuito 2
+// let condition = true
+// let nombre = prompt ("ingresar su nombre: ")
+// let edad = parseInt(prompt("Ingrese su edad:"))
+// while (condition) {
+//    if (edad>= 18) {
+//        condition = false
+//    } else {
+//        alert("no puede visitar este sitio")
+//        edad = parseInt(prompt("ingrese nueva edad: "))
+//    }
+// }
+// function darGracias() {
+//     alert("Bienvenido" + " " + nombre + "" + "usted es mayor de edad y puede seguir navegando")
+// }
+// darGracias()

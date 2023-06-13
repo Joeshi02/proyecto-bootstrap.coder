53c5de838e2991180227cdf4c1a455bb365687d7
joacoEscobar <joaco.escobar.h@gmail.com>
2023-06-13 18:19:35 -0300

    arreglos js

--- nosotros.js
+++ nosotros.js
@@ -1,5 +1,16 @@
-let entrada = prompt ("ingresar un dato")
-while (entrada !== "ESC") {
-    alert ('el usuario ingreso: '+ entrada)
-    entrada = prompt ('ingresar otro dato: ')
+let condition = true
+let nombre = prompt ("ingresar su nombre: ")
+let edad = parseInt(prompt("Ingrese su edad:"))
+while (condition) {
+   if (edad>= 18) {
+       condition = false
+   } else {
+       alert("no puede visitar este sitio")
+       edad = parseInt(prompt("ingrese nueva edad: "))
+   }
 }
+function darGracias() {
+    alert('Bienvenido' + " " + nombre + " " + 'usted es mayor de edad y puede seguir navegando')
+}
+darGracias()
+

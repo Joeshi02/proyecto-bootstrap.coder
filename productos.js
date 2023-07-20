0938d2ca2cf65fb8cc4e55e8dfae6231d9ed78ef
joacoEscobar <joaco.escobar.h@gmail.com>
2023-07-20 10:39:10 -0400

    boton

--- productos.js
+++ productos.js
@@ -1,30 +1,33 @@
-class Product {
-  constructor(title, description, brand, price) {
-    this.title = title;
-    this.description = description;
-    this.brand = brand;
-    this.price = price;
-  }
-
-  precioMasIva() {
-    this.price = this.price * 1.3;
-  }
-}
-
-let products = [];
-let cantidadDeProductos = Number(prompt("Ingrese la cantidad de productos que se va a agregar: "));
-
-for (let index = 0; index < cantidadDeProductos; index++) {
-  let title = prompt("Ingrese el nombre del producto: ");
-  let description = prompt("Ingrese la descripción del producto: ");
-  let brand = prompt("Ingrese la marca del producto: ");
-  let price = parseFloat(prompt("Ingrese el precio del producto: "));
-
-  let product = new Product(title, description, brand, price);
-  product.precioMasIva();
-  products.push(product);
-}
-
-console.log(products);
-
-alert ("El producto fue ingresado con exito.")
+let boton = document.getElementById ('btnAgregar')
+                boton.onclick = function () {
+                  class Product {
+                    constructor(title, description, brand, price) {
+                      this.title = title;
+                      this.description = description;
+                      this.brand = brand;
+                      this.price = price;
+                    }
+                  
+                    precioMasIva() {
+                      this.price = this.price * 1.3;
+                    }
+                  }
+                  
+                  let products = [];
+                  let cantidadDeProductos = Number(prompt("Ingrese la cantidad de productos que se va a agregar: "));
+                  
+                  for (let index = 0; index < cantidadDeProductos; index++) {
+                    let title = prompt("Ingrese el nombre del producto: ");
+                    let description = prompt("Ingrese la descripción del producto: ");
+                    let brand = prompt("Ingrese la marca del producto: ");
+                    let price = parseFloat(prompt("Ingrese el precio del producto: "));
+                  
+                    let product = new Product(title, description, brand, price);
+                    product.precioMasIva();
+                    products.push(product);
+                  }
+                  
+                  console.log(products);
+                  
+                  alert ("El producto fue ingresado con exito.")
+                }

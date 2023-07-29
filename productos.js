858ad4f1701bb6806ecf0913c8b963ef04e28401
joacoEscobar <joaco.escobar.h@gmail.com>
2023-07-29 16:39:24 -0400

    arreglos del simulador

--- productos.js
+++ productos.js
@@ -1,15 +1,15 @@
 class Products {
   constructor(name, model, brand, price, amount, imageUrl) {
-      this.name = name;
-      this.model = model;
-      this.brand = brand;
-      this.price = parseFloat(price);
-      this.amount = Number(amount);
-      this.imageUrl = imageUrl;
+    this.name = name;
+    this.model = model;
+    this.brand = brand;
+    this.price = parseFloat(price);
+    this.amount = Number(amount);
+    this.imageUrl = imageUrl;
   }
 
   precioMasIva() {
-      this.price = this.price * 1.3;
+    this.price = this.price * 1.3;
   }
 }
 
@@ -42,7 +42,7 @@ form.addEventListener('submit', (event) => {
   const ultimoAgregado = productosAgregados[productosAgregados.length - 1];
   ultimoAgregado.precioMasIva();
   mostrarAgregados();
-  localStorage.setItem('productosAgregados', JSON.stringify(productosAgregados));
+  guardarEnLocalStorage();
 });
 
 // Función para  tarjetas
@@ -51,25 +51,31 @@ let containerProducts = document.getElementById('products');
 function mostrarAgregados() {
   containerProducts.innerHTML = "";
   for (const product of productosAgregados) {
-      const card = document.createElement('div');
-      card.classList.add(
-          'card',
-          'col-2', 
-          'm-4',
-          'border',
-          'border-black',
-          'cardProduct'
-      );
-      card.innerHTML = `
-          <img src="${product.imageUrl}" class="card-img-top imagenProductos" alt="producto" />
-          <div class="card-body d-flex justify-content-center flex-column">
-              <h5 class="card-title d-flex justify-content-center">${product.name}</h5>
-              <p class="card-text d-flex justify-content-center">Modelo: ${product.model}</p>
-              <p class="card-text d-flex justify-content-center">Marca: ${product.brand}</p>
-              <p class="card-text d-flex justify-content-center">Precio: ${product.price}</p>
-              <p class="card-text d-flex justify-content-center">Cantidad: ${product.amount}</p>
-              <input id="comprar" type="submit" value="comprar" class="d-flex justify-content-center comprar"/>
-          </div>`;
-      containerProducts.appendChild(card);
+    const card = document.createElement('div');
+    card.classList.add(
+      'card',
+      'col-2', 
+      'm-4',
+      'border',
+      'border-black',
+      'cardProduct'
+    );
+    card.innerHTML = `
+      <img src="${product.imageUrl}" class="card-img-top imagenProductos" alt="producto" />
+      <div class="card-body d-flex justify-content-center flex-column">
+        <h5 class="card-title d-flex justify-content-center">${product.name}</h5>
+        <p class="card-text d-flex justify-content-center">Modelo: ${product.model}</p>
+        <p class="card-text d-flex justify-content-center">Marca: ${product.brand}</p>
+        <p class="card-text d-flex justify-content-center">Precio: ${product.price}</p>
+        <p class="card-text d-flex justify-content-center">Cantidad: ${product.amount}</p>
+        <input id="comprar" type="submit" value="comprar" class="d-flex justify-content-center comprar"/>
+      </div>`;
+    containerProducts.appendChild(card);
   }
 }
+
+function guardarEnLocalStorage() {
+  localStorage.setItem('productosAgregados', JSON.stringify(productosAgregados));
+}
+
+mostrarAgregados();
